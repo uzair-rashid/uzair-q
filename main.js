930ce2228360946201911b2019b6dec6c12c3f1c
uzair-rashid <uzairrashid246@gmail.com>
2024-12-16 18:13:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Search functionlity
+function filter(){
+    var filtervalue, input, productlist,productname,h4,i;input=document.getElementById("search")
+;filtervalue=input.vlaue.toUpperCase();productlist
